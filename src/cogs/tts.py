8edea13943d61c4d268b6b3bf2cc550b69182abd
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-07 09:05:16 +0000

    initial commit

--- src/cogs/tts.py
+++ src/cogs/tts.py
@@ -11,29 +11,56 @@ ENDPOINT: str = getenv("ENDPOINT")
 
 
 class TTSCog(commands.Cog):
-
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self._session = ClientSession()
+        self._channels: list[discord.TextChannel] = []
 
     async def tts(self, text: str) -> bytes:
-        async with self._session.post(f"{ENDPOINT}/audio_query", params={
-            "text": text,
-            "speaker": 1,
-        }) as aqr:
+        async with self._session.post(
+            f"{ENDPOINT}/audio_query",
+            params={
+                "text": text,
+                "speaker": 1,
+            },
+        ) as aqr:
             data = await aqr.json()
             data["outputStereo"] = False
             data["outputSamplingRate"] = 48000
-            async with self._session.post(f"{ENDPOINT}/synthesis", params={
-                "speaker": 1,
-            }) as res:
+            async with self._session.post(
+                f"{ENDPOINT}/synthesis",
+                params={
+                    "speaker": 1,
+                },
+                json=data,
+            ) as res:
                 return await res.read()
 
-    @app_commands.command()
+    @app_commands.command(description="読み上げを開始します。")
     async def join(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
         voice_client = await interaction.user.voice.channel.connect(cls=VoiceClient)
-        track = await voice_client.source(await self.tts("接続しました"))
+        track = await voice_client.source(await self.tts("接続しました"), opus=False)
         track.play()
+        self._channels.append(interaction.channel)
+        await interaction.followup.send("Hello")
+
+    @app_commands.command(description="読み上げを終了します")
+    async def leave(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
+        await interaction.guild.voice_client.disconnect()
+        self._channels.remove(interaction.channel)
+        await interaction.followup.send("読み上げを終了しました。")
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+        if message.channel in self._channels:
+            track = await message.guild.voice_client.source(
+                await self.tts(message.content), opus=False
+            )
+            track.play()
 
 
 async def setup(bot: commands.Bot) -> None:
--- src/main.py
+++ src/main.py
@@ -10,14 +10,13 @@ load_dotenv()
 
 
 class MyBot(commands.Bot):
-
     async def setup_hook(self) -> None:
         await self.load_extension("jishaku")
         await self.load_extension("cogs.tts")
 
     async def on_ready(self) -> None:
         print("Now ready!")
-    
+
 
 bot = MyBot(intents=discord.Intents.all(), command_prefix="sam!")
 
